b20113524fec14a98caf13e7d9bc59df7ad7705b
hikerpig <hikerpigwinnie@gmail.com>
2026-07-26 23:11:41 +0800

    refactor(harmony): delete legacy analyzer core

--- packages/web-core/src/harmony/bundledHarmonyPrimaryMlp.ts
+++
@@ -1,4 +0,0 @@
-import bundledModelJson from "./harmony-primary-mlp-model.json";
-import { mlpHarmonyRerankerModelSchema } from "./mlpReranker";
-
-export const bundledHarmonyPrimaryMlp = mlpHarmonyRerankerModelSchema.parse(bundledModelJson);
